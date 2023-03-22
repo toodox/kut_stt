bdc69390a05272b25f6c3e3027ba80dcb2a922ec
Carpediem324 <101110020+Carpediem324@users.noreply.github.com>
2023-03-22 14:42:01 +0900

    3/22 2시41분 아마 최종
    3/22 2시41분 아마 최종

--- STT_page_0313/public/assets/js/유의사항.js
+++ STT_page_0313/public/assets/js/유의사항.js
@@ -5,7 +5,7 @@ var speechRecognizer = new webkitSpeechRecognition();
 var constraints = { audio: true };
 var mediaRecorder;
 var chunks = [];
-var blob ;
+var blob;
 var bloburl;
 
 function startRecording() {
@@ -16,6 +16,10 @@ function startRecording() {
 
       mediaRecorder.ondataavailable = function(e) {
         chunks.push(e.data);
+        if (mediaRecorder.state == 'inactive') {
+          blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+          // Do something with the blob object, such as uploading it to the server
+        }
       }
 
       mediaRecorder.start();
@@ -25,71 +29,54 @@ function startRecording() {
     });
 }
 
-// function stopRecording() {
-//   mediaRecorder.stop();
-//   var blob = new Blob(chunks, { 'type' : 'audio/mp3; codecs=opus' });
-//   // Do something with the blob, e.g. upload it to a server
-// }
-
 function startConverting() {
-    startRecording();
-    isRecoding = true;
-    r.innerHTML = '';
-    if ('webkitSpeechRecognition' in window) {
-        speechRecognizer.continuous = true;
-        speechRecognizer.interimResults = true;
-        speechRecognizer.lang = 'ko-KR';
-        speechRecognizer.start();
-        
-        var finalTranscripts = '';
+  startRecording();
+  isRecoding = true;
+  r.innerHTML = '';
+  if ('webkitSpeechRecognition' in window) {
+    speechRecognizer.continuous = true;
+    speechRecognizer.interimResults = true;
+    speechRecognizer.lang = 'ko-KR';
+    speechRecognizer.start();
 
-        speechRecognizer.onresult = function (event) {
-            var interimTranscripts = '';
-            for (var i = event.resultIndex; i < event.results.length; i++) {
-                var transcript = event.results[i][0].transcript;
-                transcript.replace("\n", "<br>");
-                if (event.results[i].isFinal) {
-                    finalTranscripts += transcript;
-                }
-                else {
-                    interimTranscripts += transcript;
-                }
-            }
-            r.innerHTML = finalTranscripts + interimTranscripts;
-        };
+    var finalTranscripts = '';
 
-        speechRecognizer.onerror = function (event) {
+    speechRecognizer.onresult = function(event) {
+      var interimTranscripts = '';
+      for (var i = event.resultIndex; i < event.results.length; i++) {
+        var transcript = event.results[i][0].transcript;
+        transcript.replace("\n", "<br>");
+        if (event.results[i].isFinal) {
+          finalTranscripts += transcript;
+        } else {
+          interimTranscripts += transcript;
+        }
+      }
+      r.innerHTML = finalTranscripts + interimTranscripts;
+    };
 
-        };
-
-    }
-    else {
-        r.innerHTML = 'Your browser is not supported. If google chrome, please upgrade!';
-    }
+    speechRecognizer.onerror = function(event) {};
+  } else {
+    r.innerHTML = 'Your browser is not supported. If google chrome, please upgrade!';
+  }
 }
 
 function stopConverting() {
-
-    speechRecognizer.stop();
-    mediaRecorder.stop();
-    blob = new Blob(chunks, {
-        'type': 'audio/mp3 codecs=opus'
-    })
-
+  speechRecognizer.stop();
+  mediaRecorder.stop();
 }
+
 var db = firebase.firestore();
 var storage = firebase.storage();
 
-
-$('#send').click(function () {
+$('#send').click(function() {
   var 저장할거 = {
-      내용: $('#content').val(),
-      날짜: new Date(),
-      유저명: "user",
-  }
+    내용: $('#content').val(),
+    날짜: new Date(),
+    유저명: "user",
+  };
 
-
-  if (저장할거.내용 != '' ) {
+  if (저장할거.내용 != '') {
     var ok = window.confirm("전송하시겠습니까?");
     if (ok) {
       var storageRef = storage.ref();
@@ -97,7 +84,7 @@ $('#send').click(function () {
       var 업로드작업 = 저장할경로.put(blob);
 
       db.collection('teststt').add(저장할거).then((result) => {
-        window.location.href = '/모의면접.html'
+        window.location.href = '/모의면접.html';
         console.log(result)
         alert("정상동작 하였습니다.");
       }).catch((error) => {
