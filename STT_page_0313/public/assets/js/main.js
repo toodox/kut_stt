4462a4bcd81d94603be8e96ef5f17d99d70e5fd3
kimgigang <114456639+kimgigang@users.noreply.github.com>
2023-03-29 14:09:36 +0900

    Revert "전부 로그인처리"
    This reverts commit 43a38fc83bb58fabb3dbee4750631fc33cf9db8e.

---
+++ STT_page_0313/public/assets/js/main.js
@@ -0,0 +1,12 @@
+// firebase.auth().onAuthStateChanged(function(user) {
+//     if (user) {
+//       // 로그인 된 경우
+//       document.getElementById('loginbtn').innerHTML = 'LogOUT';
+//       document.getElementById('userInfo').innerHTML = user.displayName + '님 환영합니다!';
+//     } else {
+//       // 로그아웃 된 경우
+//       document.getElementById('loginBtn').innerHTML = '로그인';
+//       document.getElementById('userInfo').innerHTML = '로그인이 필요합니다.';
+//     }
+//   });
+
