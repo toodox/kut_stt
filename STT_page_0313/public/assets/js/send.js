38949d331f6a4c3cb579a03e6ce493cb7d9b16cf
kimgigang <114456639+kimgigang@users.noreply.github.com>
2023-05-03 15:42:23 +0900

    자잘한 부분 수정

--- STT_page_0313/public/assets/js/send.js
+++ STT_page_0313/public/assets/js/send.js
@@ -3,9 +3,10 @@
 // }
 
 // 전달할 데이터
-const email = document.getElementById('userem');
-console.log(email.textContent);
-
-email.addEventListener('click', () => {
-    localStorage.setItem("email",email.textContent);
+// us 클래스인  a태그가 클릭되면
+$(".us").on('click', function (e) {
+    console.log(e.target.id)
+    const email = document.getElementById(e.target.id);     //클릭된 a태그의 id를 가져온다
+    console.log(email.textContent);
+    localStorage.setItem("email", email.textContent);
 });
