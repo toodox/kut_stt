2777237b6f69f16ef4730a933cf54ed57198dfdf
Carpediem324 <101110020+Carpediem324@users.noreply.github.com>
2023-05-17 17:22:59 +0900

    상세결과 전 선택 창 추가 상세결과 수정
    상세결과 전 선택 창 추가 상세결과 수정

--- STT_page_0313/functions/public/assets/js/유형선택.js
+++ STT_page_0313/functions/public/assets/js/유형선택.js
@@ -1,17 +1,12 @@
-document.getElementById("typeA").addEventListener("click", function() {
-    saveSelectedType("A");
+const buttons = ["typeA", "typeB", "typeC"];
+
+buttons.forEach(function (type) {
+  document.getElementById(type).addEventListener("click", function () {
+    saveSelectedType(type);
   });
-  
-  document.getElementById("typeB").addEventListener("click", function() {
-    saveSelectedType("B");
-  });
-  
-  document.getElementById("typeC").addEventListener("click", function() {
-    saveSelectedType("C");
-  });
-  
-  function saveSelectedType(type) {
-    localStorage.setItem("selectedType", type); // 웹 브라우저에 변수를 저장
-    window.location.href = "/notice"; // 유의사항.html 페이지로 이동
-  }
-  
+});
+
+function saveSelectedType(type) {
+  localStorage.setItem("selectedType", type); // 웹 브라우저에 변수를 저장
+  window.location.href = "/notice"; // 유의사항.html 페이지로 이동
+}
