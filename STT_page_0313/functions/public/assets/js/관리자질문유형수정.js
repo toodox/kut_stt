03b21935f0190add0a475b4523abe7e3ae79b773
kimgigang <114456639+kimgigang@users.noreply.github.com>
2023-05-22 21:40:28 +0900

    대영 질문지 리스트 불러오기

--- STT_page_0313/functions/public/assets/js/관리자질문유형수정.js
+++ STT_page_0313/functions/public/assets/js/관리자질문유형수정.js
@@ -19,15 +19,15 @@ xhr.onreadystatechange = () => {
                 let que = document.getElementById('queli');
                 let newdiv = document.createElement('div');
                 let itype = name.split('_', 2);
-                newdiv.innerHTML = '<div class="accordion-item"><h2 class="accordion-header" id="panelsStayOpen-heading' + itype[1] + '"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"data-bs-target="#panelsStayOpen-collapse' + itype[1] + '" aria-expanded="false"aria-controls="panelsStayOpen-collapse' + itype[1] + '">' + name + '</button></h2><div id="panelsStayOpen-collapse' + itype[1] + '" class="accordion-collapse collapse"aria-labelledby="panelsStayOpen-heading' + itype[1] + '"><div class="accordion-body" id="qnum"><div class="card my-4"><div class="card-body text-center" id="qqq' + itype[1] + '"></div></div></div></div></div>';
+                newdiv.innerHTML = '<div class="accordion-item"><h2 class="accordion-header" id="panelsStayOpen-heading' + itype[1] + '"><button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"data-bs-target="#panelsStayOpen-collapse' + itype[1] + '" aria-expanded="false"aria-controls="panelsStayOpen-collapse' + itype[1] + '">' + name + '</button></h2><div id="panelsStayOpen-collapse' + itype[1] + '" class="accordion-collapse collapse"aria-labelledby="panelsStayOpen-heading' + itype[1] + '"><div class="accordion-body" id="qnum"><div class="card my-4 "><div class="card-body" id="qqq' + itype[1] + '"></div></div><button>테스트버튼</button></div></div></div>';
                 que.append(newdiv);
                 db.collection(name).get().then(snap => {
 
                     for (let quenum = 1; quenum < snap.size + 1; quenum++) {
                         db.collection(name).doc(itype[1] + '_' + itype[0] + quenum).get().then((result) => {
                             let chp = document.getElementById('qqq' + itype[1]);
-                            let newp = document.createElement('textarea');
-                            newp.innerHTML = result.data().content;
+                            let newp = document.createElement('div');
+                            newp.innerHTML = itype[1] + '_' + itype[0] + quenum+'<textarea class="admintextarea mb-2">'+result.data().content+'</textarea>';
                             chp.append(newp);
                         });
                     }
