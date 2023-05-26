91367886a8b4456269694fe7aebbc6cd4ca3c929
Carpediem324 <101110020+Carpediem324@users.noreply.github.com>
2023-05-26 15:23:07 +0900

    알림창 sweetalert코드 추가
    알림창 sweetalert코드 추가

--- STT_page_0313/functions/public/assets/js/nav로그인.js
+++ STT_page_0313/functions/public/assets/js/nav로그인.js
@@ -4,12 +4,12 @@ document.getElementById(`mockinterview`).addEventListener('click', function() {
             window.location.href = '/select-type';
         } else {
             swal({
-                title: "Hello",
+                title: "Login",
                 text: "로그인이 필요한 서비스입니다.",
-                icon: "info", //"info,success,warning,error" 중 택1
+                icon: "warning", //"info,success,warning,error" 중 택1
             }).then((ok) => {
                 if (ok) {
-                    window.location.href = '/login';
+                    window.location.href = '/Login';
                 /* "YES"클릭시 로직 */
                 }
            });
@@ -23,9 +23,9 @@ document.getElementById(`result`).addEventListener('click', function() {
             window.location.href = '/choiceresults';
         } else {
             swal({
-                title: "Hello",
+                title: "Login",
                 text: "로그인이 필요한 서비스입니다.",
-                icon: "info", //"info,success,warning,error" 중 택1
+                icon: "warning", //"info,success,warning,error" 중 택1
             }).then((ok) => {
                 if (ok) {
                     window.location.href = '/login';
