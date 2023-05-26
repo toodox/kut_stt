7c87adec4e9c0c3549272fa90968269dfb7aa46d
Carpediem324 <101110020+Carpediem324@users.noreply.github.com>
2023-05-26 15:16:41 +0900

    sweetalert사용
    sweetalert사용

--- STT_page_0313/functions/public/assets/js/nav로그인.js
+++ STT_page_0313/functions/public/assets/js/nav로그인.js
@@ -3,8 +3,16 @@ document.getElementById(`mockinterview`).addEventListener('click', function() {
         if (user) {
             window.location.href = '/select-type';
         } else {
-            alert('로그인이 필요한 서비스입니다.');
-            window.location.href = '/login';
+            swal({
+                title: "Hello",
+                text: "로그인이 필요한 서비스입니다.",
+                icon: "info", //"info,success,warning,error" 중 택1
+            }).then((ok) => {
+                if (ok) {
+                    window.location.href = '/login';
+                /* "YES"클릭시 로직 */
+                }
+           });
         }
     });
 });
@@ -14,8 +22,16 @@ document.getElementById(`result`).addEventListener('click', function() {
         if (user) {
             window.location.href = '/choiceresults';
         } else {
-            alert('로그인이 필요한 서비스입니다.');
-            window.location.href = '/login';
+            swal({
+                title: "Hello",
+                text: "로그인이 필요한 서비스입니다.",
+                icon: "info", //"info,success,warning,error" 중 택1
+            }).then((ok) => {
+                if (ok) {
+                    window.location.href = '/login';
+                /* "YES"클릭시 로직 */
+                }
+           });
         }
     });
 });
